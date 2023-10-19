93a151bcc8d8d2fb88e8a0daad40ac6eb9a29084
Gdngkp <130331691+Gdngkp@users.noreply.github.com>
2023-10-19 14:47:30 +0700

    gading

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
-<?php
-// index.php
-
-// Get the path from the URL
-$path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
-
-// Your logic to handle different paths
-switch ($path) {
-    case '/':
-        echo 'Home Page';
-        break;
-        case '/about':
-            echo 'About Page';
-        
-        break;
-    // Add more cases as needed
-    default:
-        echo '404 Not Found';
-        break;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menfess</title>
+</head>
+<body>
+    <h1>menfess index</h1>
+</body>
+</html>
