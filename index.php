c60a37e349934ac8ccbdb6665c2b1c13b7931620
DhitGT <adityadwisaputra666@gmail.com>
2023-10-22 15:34:39 +0700

    index page

--- index.php
+++ index.php
@@ -1,11 +1,217 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>menfess</title>
+    <title>Message</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>menfess index</h1>
+    <header>
+        <nav>
+            <div class="logo">
+                <h3>Message</h3>
+
+            </div>
+            <ul>
+                <li>
+                    <a onclick="scrollToSection('bodys')">About</a>
+                </li>
+                <li>
+                    <a onclick="scrollToSection('menfess')">Menfess</a>
+                </li>
+                <li>
+                    <a onclick="scrollToSection('ngl')">Ngl</a>
+                </li>
+                <li>
+                    <a href="">Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container">
+        <section id="bodys">
+            <div class="body-1">
+                <p class="brand">Message</p>
+                <div class="badge">
+                    <div class="badge-item">ANONYMOUS</div>
+                    <div class="badge-item">MESSAGE</div>
+                    <div class="badge-item">PRIVACY</div>
+                </div>
+                <div class="quotes">
+                    <p align="justify">
+                        Silent Notes is a website where you can send messages to anyone anonymously. With this platform, you have the ability to communicate without revealing your identity, creating a secure and traceless space for conversation. It's the perfect place to share thoughts, feelings, and messages without the worry of identity disclosure.
+                    </p>
+                </div>
+                <p class="info-txt">Explore More</p>
+                <div class="destination">
+                    <a onclick="scrollToSection('about')" class="desti-item"> MENFES </a>
+                    <a onclick="scrollToSection('project')" class="desti-item"> SEND SECRET MAIL </a>
+                </div>
+            </div>
+            <div class="body-2">
+        
+                <div class="bg-img"></div>
+            </div>
+        </section>
+
+    </div>
+    <section id="menfess">
+        <div class="container">
+            <div class="card card-menfess">
+                <div class="menfess-left">
+                    <div class="menfess-example">
+                        <div>
+                            <div class="exam-head">
+                                <b>
+                                    <p class="to"></p>
+                                </b>
+                                <hr>
+                            </div>
+                            <div class="exam-body">
+                                <span class="message">
+
+                                </span>
+                                <b>
+                                    <p class="from"></p>
+                                </b>
+                                <hr>
+                            </div>
+                        </div>
+                        <div class="exam-bottom">
+                            <div class="icon">
+                                like
+                            </div>
+                            <div class="icon">
+                                comment
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="menfess-right">
+                    <div class="menfess-right-info">
+                        <h2>Menfess</h2>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet expedita a modi ad ea obcaecati sunt nostrum adipisci assumenda quis voluptas debitis praesentium eum autem, quam quos corporis dicta, cupiditate veniam placeat explicabo deleniti odit, est laborum? Rem, labore vel. Animi error quibusdam tenetur omnis?</p>
+                    </div>
+                    <div class="action">
+                        <div class="action-item">
+                            <a href="" class="btn btn-info">Lihat Lebih Banyak Menfess</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section id="ngl">
+        <div class="container">
+            <div class="ngl-wrapper">
+                <div class="ngl-top">
+                    <h1>Not Gonna Lie</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, numquam! Eaque officiis, inventore iste, enim cupiditate aut blanditiis fugiat ratione amet omnis voluptatum autem maiores, eius aperiam quaerat! Voluptates natus sit eius! Fuga, officia?</p>
+                </div>
+                <div class="ngl-bottom">
+                    <h4 align="center">Ngl Theme</h4>
+                    <p align="center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio adipisci veritatis, ex tempora esse iste sequi voluptatum numquam magnam in?</p>
+                    <div class="ngl-theme">
+                        <div class="theme-item">
+                            <div class="theme-top">
+                                <h5>Kirimi aku pesan anonim</h5>
+                                <hr>
+                            </div>
+                            <div class="theme-bottom">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum amet mollitia animi. Facere commodi aliquid iusto molestias dolor! Voluptates incidunt dolores </p>
+                            </div>
+                            <a href="" class="btn btn-primary mt-auto">Buat Sekarang</a>
+                        </div>
+                        <div class="theme-item">
+                            <div class="theme-top">
+                                <h5>Confession</h5>
+                                <hr>
+                            </div>
+                            <div class="theme-bottom">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum amet mollitia animi. Facere commodi aliquid iusto molestias dolor! Voluptates incidunt dolores id, sapiente fuga atque labore provident nobis est nam.</p>
+                            </div>
+                            <a href="" class="btn btn-primary mt-auto">Buat Sekarang</a>
+                        </div>
+                        <div class="theme-item">
+                            <div class="theme-top">
+                                <h5>3 Fact About Me</h5>
+                                <hr>
+                            </div>
+                            <div class="theme-bottom">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum amet mollitia animi. Facere commodi aliquid iusto molestias dolor! Voluptates incidunt dolores id, sapiente fuga atque labore provident nobis est nam.</p>
+                            </div>
+                            <a href="" class="btn btn-primary mt-auto">Buat Sekarang</a>
+                        </div>
+                        <div class="theme-item">
+                            <div class="theme-top">
+                                <h5>Your crush</h5>
+                                <hr>
+                            </div>
+                            <div class="theme-bottom">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum amet mollitia animi. Facere commodi aliquid iusto molestias dolor! Voluptates incidunt dolores id, sapiente fuga atque labore provident nobis est nam.</p>
+                            </div>
+                            <a href="" class="btn btn-primary mt-auto">Buat Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="footer-group">
+            <div class="footer-1">  
+                <h5>Contact</h5>
+                <ul>
+                    <li>
+                        <a href="mailto:dheep.co@gmail.com">dheep.co@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-2">
+                <h5>Powered By</h5>
+                <ul>
+                    <li>
+                        <p>Boostrap 5</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-3">
+                <h5>Social Media</h5>
+                <ul>
+                    <li>
+                        <p>instagram : @d.dheepy</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <p align="center">&copy; dheep.site All Rights Reserved</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        // JavaScript function to scroll to a section with an offset
+        function scrollToSection(sectionId) {
+            const section = document.getElementById(sectionId);
+
+            if (section) {
+                // Calculate the offset (adjust as needed)
+                const offset = 100;
+
+                // Scroll to the section with the offset
+                window.scrollTo({
+                    top: section.offsetTop - offset,
+                    behavior: "smooth",
+                });
+            }
+        }
+    </script>
+
+
 </body>
+
 </html>
---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $username = 'root';
+    $password = '';
+    $server = 'localhost';
+    $dbName = 'db_secret_message';
+
+
+?>
